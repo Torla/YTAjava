2ff225c0b881ebe23ac81c8df5a5b425f3836d83
torla <marcotorlaschi95@gmail.com>
2019-03-28 22:57:21 +0100

    dataStreamEx

---
+++ src/DataEx.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class DataEx {
+  public static void main(String[] args) {
+    final String nomeFile = "fileDiProvaData";
+    File file = new File(nomeFile);
+    DataOutputStream dataOutput=null;
+    try {
+      dataOutput = new DataOutputStream(new FileOutputStream(file)) ;
+      dataOutput.writeInt(101);
+      dataOutput.writeInt(102);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    finally {
+      try {if(dataOutput!=null)dataOutput.close();} catch (IOException e){}
+    }
+
+    DataInputStream dataInputStream=null;
+    try{
+      dataInputStream = new DataInputStream(new FileInputStream(nomeFile));
+      int b;
+      while(true){
+        b = dataInputStream.readInt();
+        System.out.println(b);
+      }
+    } catch (EOFException e){} catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {if(dataInputStream!=null)dataInputStream.close();} catch (IOException e){}
+    }
+  }
+}
--- src/FileEx.java
+++ src/FileEx.java
@@ -1,35 +1,35 @@
 
-import java.io.*;
+  import java.io.*;
 
-public class FileEx{
+  public class FileEx{
 
-    public static void main(String[] args) {
-    	final String nomeFile = "fileDiProva";
-	    File file = new File(nomeFile);
-	    BufferedWriter bufferedWriter=null;
-	    try {
-		    bufferedWriter = new BufferedWriter(new FileWriter(file)) ;
-		    bufferedWriter.write("Prova\n");
+      public static void main(String[] args) {
+        final String nomeFile = "fileDiProva";
+        File file = new File(nomeFile);
+        BufferedWriter bufferedWriter=null;
+        try {
+          bufferedWriter = new BufferedWriter(new FileWriter(file)) ;
+          bufferedWriter.write("Prova\n");
 
-	    } catch (IOException e) {
-		    e.printStackTrace();
-	    }
-	    finally {
-		    try {if(bufferedWriter!=null)bufferedWriter.close();} catch (IOException e){}
-	    }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        finally {
+          try {if(bufferedWriter!=null)bufferedWriter.close();} catch (IOException e){}
+        }
 
-	    BufferedReader bufferedReader=null;
-	    try{
-	    	bufferedReader = new BufferedReader(new FileReader(nomeFile));
-	    	String line=null;
-		    while((line = bufferedReader.readLine()) != null){
-			    System.out.println(line);
-		    }
-	    } catch (IOException e) {
-		    e.printStackTrace();
-	    }
-	    finally {
-		    try {if(bufferedReader!=null)bufferedReader.close();} catch (IOException e){}
-	    }
-	  }
-}
+        BufferedReader bufferedReader=null;
+        try{
+          bufferedReader = new BufferedReader(new FileReader(nomeFile));
+          String line=null;
+          while((line = bufferedReader.readLine()) != null){
+            System.out.println(line);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        finally {
+          try {if(bufferedReader!=null)bufferedReader.close();} catch (IOException e){}
+        }
+      }
+  }
